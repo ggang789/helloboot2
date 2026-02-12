bd77042cf44060a0a0e04b8259efef52a11945d8
JR <sjlee@cookat.co>
2026-02-12 09:21:37 +0900

    complete

--- src/test/java/tobyspring/helloboot/HelloRepositoryTest.java
+++ src/test/java/tobyspring/helloboot/HelloRepositoryTest.java
@@ -3,14 +3,16 @@ package tobyspring.helloboot;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@HellobootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Transactional
 public class HelloRepositoryTest {
 
     @Autowired
